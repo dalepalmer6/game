95b9d433bb8d7b4696a3d12762c5f44d80695a90
dalepalmer6 <dalepalmer6@gmail.com>
2019-04-30 17:23:19 -0230

    various bug fixes and improvements to anims

---
+++ maps/house - myhome/01.cs
@@ -0,0 +1,20 @@
+lamp
+text,BGM change! [SETBGM_poltergeist.ogg]Success!
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+text,OH MY SWEET JESUS THE LAMP STARTED TO MOVE!!![STARTBATTLE_Lamp]Battle
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+move,-8,0
+text,Somehow the lamp became tame.
+move,-8,0
