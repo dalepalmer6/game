ea132e981d981b7d7ebff940110f17bfe2110f6d
dalepalmer6 <dalepalmer6@gmail.com>
2019-05-12 11:48:30 -0230

    bunch of map updates

--- maps/house - myhome/01.cs
+++ maps/house - myhome/01.cs
@@ -1,20 +1,5 @@
 lamp
-text,BGM change! [SETBGM_poltergeist.ogg]Success!
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-text,The lamp began to move![PROMPTINPUT] [STARTBATTLE_Lamp] [SETFLAG_lampDead] 
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
-move,-8,0
+move,2032,2512
+text,The lamp began to move![PROMPTINPUT] [SETFLAG_lampDead] 
 text,Somehow the lamp became tame.
-move,-8,0
+move,2100,2560
