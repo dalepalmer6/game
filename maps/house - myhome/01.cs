4569b7677cb220ae6e6a9dec726311811d0fcc5d
dalepalmer6 <dalepalmer6@gmail.com>
2019-06-20 22:02:04 -0230

    jun19

--- maps/house - myhome/01.cs
+++ maps/house - myhome/01.cs
@@ -1,3 +1,3 @@
 lamp
-text,The lamp began to move![PROMPTINPUT][STARTBATTLE_2] [SETFLAG_lampDead] 
-text,Somehow the lamp became tame.
+text,The lamp began to move![PROMPTINPUT][STARTBATTLE_2]  
+text,Somehow the lamp became tame.[SETFLAG_lampDead] 
